e31c086740e4a23a9aea1730f8fc341c5a45ce1f
jadend-r <jadenreid1125@gmail.com>
2025-05-01 19:16:57 -0400

    fixing s3 bucket

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -2,6 +2,10 @@
 resource "aws_s3_bucket" "frontend" {
   bucket = "aws-job-tracker-frontend-prod" 
   force_destroy = true              # Destroys bucket even if non-empty
+
+   lifecycle {
+    ignore_changes = [website]
+  }
 }
 
 # Enable static website hosting
