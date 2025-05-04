ada4b0075d55f5a7e91b8d7ddfc07216c6031ce4
jadend-r <jadenreid1125@gmail.com>
2025-05-03 23:28:22 -0400

    Updated login page

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,11 +1,14 @@
+import { useNavigate } from "react-router-dom"
+
 const Login = () => {
+    const navigate = useNavigate();
 
     return (
         <div>
             <h1>Login page</h1>
-            <span className={`px-2 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-700`}>
+            <button className={`px-2 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-700`} onClick={() => {navigate("https://job-tracker-app-744ivu.auth.us-east-1.amazoncognito.com/login?response_type=token&client_id=7s2vj2fch4c8flnu5199p2q6in&redirect_uri=https://dcli3b34ssmw2.cloudfront.net/login-redirect")}}>
                 Login
-            </span>
+            </button>
         </div>
     )
 }
