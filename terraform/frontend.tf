84ee7d2f2ad308f6731433b7d8516d6f0913a6f1
jadend-r <jadenreid1125@gmail.com>
2025-05-03 22:35:13 -0400

    added CloudFront

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -46,6 +46,57 @@ resource "aws_s3_bucket_policy" "frontend" {
   depends_on = [aws_s3_bucket_public_access_block.frontend]
 }
 
-output "website_url" {
-  value = "http://${aws_s3_bucket_website_configuration.frontend.website_endpoint}"
+## Configure CloudFront
+
+resource "aws_cloudfront_origin_access_control" "frontend" {
+  name                              = "s3-oac-frontend"
+  origin_access_control_origin_type = "s3"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+}
+
+resource "aws_cloudfront_distribution" "frontend" {
+  enabled             = true
+  default_root_object = "index.html"
+
+  origin {
+    domain_name = aws_s3_bucket.frontend.bucket_regional_domain_name
+    origin_id   = "frontendS3"
+
+    origin_access_control_id = aws_cloudfront_origin_access_control.frontend.id
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "frontendS3"
+
+    viewer_protocol_policy = "redirect-to-https"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+    minimum_protocol_version       = "TLSv1.2_2021"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  tags = {
+    Name = "frontend-distribution"
+  }
+}
+
+output "cloudfront_domain" {
+  value = aws_cloudfront_distribution.frontend.domain_name
 }
