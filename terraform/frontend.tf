1e5d82a8dea9bb47e716a974da771867f00ca6c2
jadend-r <jadenreid1125@gmail.com>
2025-05-03 23:47:19 -0400

    updated cloudfront s3 bucket policy name

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -46,7 +46,7 @@ resource "aws_s3_bucket_policy" "frontend" {
   depends_on = [aws_s3_bucket_public_access_block.frontend]
 }
 
-resource "aws_s3_bucket_policy" "frontend" {
+resource "aws_s3_bucket_policy" "cloudfront-frontend" {
   bucket = aws_s3_bucket.frontend.id
 
   policy = jsonencode({
