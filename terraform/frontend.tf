6509e37284b595e2ff67f8c6aa5ed97ea70aac1a
jadend-r <jadenreid1125@gmail.com>
2025-05-05 13:00:38 -0400

    changed s3 config for cloudfront access only

--- terraform/frontend.tf
+++ terraform/frontend.tf
@@ -4,29 +4,16 @@ resource "aws_s3_bucket" "frontend" {
   force_destroy = true              # Destroys bucket even if non-empty
 }
 
-# Enable static website hosting
-resource "aws_s3_bucket_website_configuration" "frontend" {
-  bucket = aws_s3_bucket.frontend.id
-
-  index_document {
-    suffix = "index.html"
-  }
-
-  error_document {
-    key = "index.html"
-  }
-}
-
-# Allow public access to S3 bucket objects
+# Deny public access to S3 bucket objects
 resource "aws_s3_bucket_public_access_block" "frontend" {
   bucket = aws_s3_bucket.frontend.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-# S3 bucket policy to allow public reads
+# S3 bucket policy to allow cloundfront access
 resource "aws_s3_bucket_policy" "frontend" {
   bucket = aws_s3_bucket.frontend.id
 
@@ -34,11 +21,18 @@ resource "aws_s3_bucket_policy" "frontend" {
     Version = "2012-10-17",
     Statement = [
       {
-        Sid = "PublicReadGetObject",
-        Effect = "Allow",
-        Principal = "*",
-        Action = "s3:GetObject",
-        Resource = "${aws_s3_bucket.frontend.arn}/*"
+        Sid: "AllowCloudFrontAccessOnly",
+        Effect: "Allow",
+        Principal: {
+          Service: "cloudfront.amazonaws.com"
+        },
+        Action: "s3:GetObject",
+        Resource: "${aws_s3_bucket.frontend.arn}/*",
+        Condition: {
+          StringEquals: {
+            "AWS:SourceArn" = aws_cloudfront_distribution.frontend.arn
+          }
+        }
       }
     ]
   })
