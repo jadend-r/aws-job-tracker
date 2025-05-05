8ab5ce6e9f57d3215f13442b49627ddffd94ec13
jadend-r <jadenreid1125@gmail.com>
2025-05-05 13:59:02 -0400

    Added authorization to CORS header

--- lambda/node/lib/response.js
+++ lambda/node/lib/response.js
@@ -1,5 +1,5 @@
 exports.defaultHeaders = {
     'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Headers': 'Content-Type',
+    'Access-Control-Allow-Headers': 'Content-Type,Authorization',
     'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
   };
