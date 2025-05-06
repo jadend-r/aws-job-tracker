0cceb971975495befef5b6b68000ff3c1469ed83
jadend-r <jadenreid1125@gmail.com>
2025-05-05 20:22:10 -0400

    added debug printouts to amplify config

--- frontend/src/amplify-auth-config.ts
+++ frontend/src/amplify-auth-config.ts
@@ -1,6 +1,11 @@
 import { Amplify } from "aws-amplify";
 import type { ResourcesConfig } from "aws-amplify";
 
+console.log("USER_POOL_ID:", process.env.REACT_APP_COGNITO_USER_POOL_ID);
+console.log("CLIENT_ID:", process.env.REACT_APP_COGNITO_USER_POOL_CLIENT_ID);
+console.log("DOMAIN:", process.env.REACT_APP_COGNITO_DOMAIN);
+
+
 const config: ResourcesConfig = {
   Auth: {
     Cognito: {
