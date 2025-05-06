2fdd4df8c5606701cbf6b807a6234d00439d0aec
jadend-r <jadenreid1125@gmail.com>
2025-05-05 20:44:56 -0400

    fix(Login page): user cannot nav to login page once logged in

--- frontend/src/pages/Login.tsx
+++ frontend/src/pages/Login.tsx
@@ -1,19 +1,38 @@
-import { signInWithRedirect } from "aws-amplify/auth"
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { fetchAuthSession } from 'aws-amplify/auth';
+import { signInWithRedirect } from 'aws-amplify/auth';
 
 const Login = () => {
-    return (
-        <div>
-            <h1>Login page</h1>
-            <button
-                className={`px-2 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-700`} 
-                onClick={() => { 
-                    signInWithRedirect()
-                }}
-            >
-                Login
-            </button>
-        </div>
-    )
-}
+  const navigate = useNavigate();
 
-export default Login
+  useEffect(() => {
+    const checkSession = async () => {
+      try {
+        const session = await fetchAuthSession();
+        const idToken = session.tokens?.idToken?.toString();
+
+        if (idToken) {
+          navigate('/dashboard');
+        }
+      } catch (err) {
+      }
+    };
+
+    checkSession();
+  }, [navigate]);
+
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <button
+        className="px-2 py-1 rounded-full text-sm font-medium bg-blue-100 text-blue-700"
+        onClick={() => signInWithRedirect()}
+      >
+        Login
+      </button>
+    </div>
+  );
+};
+
+export default Login;
